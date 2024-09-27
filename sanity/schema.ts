0613d07432332a9bb335f4adba2e3bc6f9d57842
CodeWithinferno <infernotech2618@gmail.com>
2024-09-27 14:13:42 -0400

    chore: Update import path for Work component in sanity/schema.ts

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import Image from "./Docs/Image";
-import Work from "./Docs/Work";
+import Work from "./Docs/work";
 import post from "./Docs/blog";
 import imagePost from "./Docs/sneakpeak";
 export const schema: { types: SchemaTypeDefinition[] } = {
