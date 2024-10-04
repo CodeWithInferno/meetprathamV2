3b3ce290e4b8226409a3e03faf7bffe81dcfc1ad
CodeWithinferno <infernotech2618@gmail.com>
2024-10-04 13:44:24 -0400

    chore: Update npm dependency to latest version of @vercel/speed-insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
