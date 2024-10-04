7ddbaac365be66e99c02e658d9327d913e800f56
CodeWithinferno <infernotech2618@gmail.com>
2024-10-04 13:39:52 -0400

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
